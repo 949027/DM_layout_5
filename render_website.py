c120b555e1d303abe64d05db340df72291d3f75c
949027 <82878668+949027@users.noreply.github.com>
2021-12-24 14:26:31 +0700

    Fixed server run. Numbers replaced with variables

--- render_website.py
+++ render_website.py
@@ -20,7 +20,6 @@ def on_reload(books_description, page_path, page_number, pages_amount):
     )
 
     template = env.get_template('template.html')
-    print(books_description)
     rendered_page = template.render(
         books_description=books_description,
         current_page_number=page_number,
@@ -33,27 +32,32 @@ def on_reload(books_description, page_path, page_number, pages_amount):
 
 def main():
     makedirs('pages', exist_ok=True)
+    page_books_amount = 10
+    columns_amount = 2
 
     description_path = 'description/descriptions.json'
 
     splited_all_description = list(chunked(
         load_books_description(description_path),
-        10,
+        page_books_amount,
     ))
     pages_amount = len(splited_all_description)
-
+    print(splited_all_description)
     for page_number, page_description in enumerate(
             splited_all_description,
             1,
     ):
-        splited_page_description = list(chunked(page_description, 2))
+        splited_page_description = list(chunked(
+            page_description,
+            columns_amount,
+        ))
         page_path = f'pages/index{page_number}.html'
 
         on_reload(splited_page_description, page_path, page_number, pages_amount)
 
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='.')
+    server.serve(default_filename='pages/index1.html')
 
 
 if __name__ == '__main__':
